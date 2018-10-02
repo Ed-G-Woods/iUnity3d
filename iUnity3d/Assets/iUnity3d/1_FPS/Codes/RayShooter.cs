c29a0f51294fb8dc0c0ce356b36500058c484de9
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-10-02 16:59:23 +0800

    raycasting shoot& get hit gameObject

---
+++ iUnity3d/Assets/iUnity3d/1_FPS/Codes/RayShooter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour {
+
+    private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+        _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit))    //out等于C++ &
+            {
+                GameObject hitobj = hit.transform.gameObject;
+                ReactiveTarget target = hitobj.GetComponent<ReactiveTarget>();
+
+                if (target == null)
+                {
+                    StartCoroutine(SphereIndicator(hit.point));
+                    
+                }
+                else
+                {
+                    StartCoroutine(SphereIndicator(hit.point));
+                    Debug.Log("hit " + hit.point);
+                }
+                
+            }
+        }
+	}
+
+    void OnGUI()
+    {
+        int size = 12;
+        float posX = _camera.pixelWidth / 2;
+        float posY = _camera.pixelHeight / 2;
+
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
