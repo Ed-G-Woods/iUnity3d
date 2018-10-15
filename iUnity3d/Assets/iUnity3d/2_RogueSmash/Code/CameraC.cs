e68798db70a2dbed6736139eb24416e90f761ef3
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-10-15 15:51:46 +0800

    TopDown Move & Camera

---
+++ iUnity3d/Assets/iUnity3d/2_RogueSmash/Code/CameraC.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraC : MonoBehaviour {
+
+    [SerializeField] private float OffsetH = -10;
+    [SerializeField] private float OffsetV = 20;
+    [SerializeField] private float OffsetSpeed=1;
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private bool isLookAt=true;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Offset();
+        Look();
+    }
+
+    private void Offset()
+    {
+        Vector3 target = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y + OffsetV, targetObject.transform.position.z + OffsetH);
+        Vector3 direction = target - transform.position;
+        transform.position += direction * OffsetSpeed * Time.deltaTime;
+    }
+
+    private void Look()
+    {
+        if (isLookAt)
+        {
+            transform.LookAt(targetObject.transform.position);
+        }
+    }
+}
+
---
+++ iUnity3d/Assets/iUnity3d/2_RogueSmash/Code/CharacterC.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterC: MonoBehaviour {
+
+    [SerializeField] private GameObject Player;
+    [SerializeField] private float MoveSpeed =3 ;
+
+    private Vector3 mousePosition;
+    private Vector3 lookDirection;
+
+    private void Start()
+    {
+        if (Player==null)
+        {
+            Player = gameObject;
+        }
+    }
+    
+
+    private void FixedUpdate()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        Quaternion rot = getPlayerRotation();
+        Player.transform.rotation =rot;
+
+        Vector3 move = getPlayerMove();
+        Player.transform.Translate(move * MoveSpeed * Time.deltaTime,Space.World);
+    }
+
+    private Quaternion getPlayerRotation()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 relativeMousePos = new Vector2(mousePos.x - Screen.width / 2, mousePos.y - Screen.height / 2);
+
+        float angle = Mathf.Atan2(relativeMousePos.y, relativeMousePos.x) * Mathf.Rad2Deg * -1;
+
+        Quaternion rotation  =Quaternion.AngleAxis(angle,Vector3.up);
+        return rotation;
+        
+    }
+
+    private Vector3 getPlayerMove()
+    {
+        Vector3 move = Vector3.zero;
+        move.x = Input.GetAxis("Horizontal");
+        move.z = Input.GetAxis("Vertical");
+
+        return move;
+    }
+}
